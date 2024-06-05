411eb7282b2c47bb869167ec9ef311485aaff7b8
Andreas Olsson <andreas@uxd.nu>
2024-06-05 06:53:24 +0200

    Updatering av Node och boka tid

--- src/pages/BokaTid.tsx
+++ src/pages/BokaTid.tsx
@@ -36,7 +36,7 @@ function BokaTid() {
         <IDSContainer gutterless={true} className="ids-content">
           <IDSRow gap="2rem" justify={"space-between"}>
             <IDSCol cols="8" m="12" s="12" className="ids-content ids-mt-6">
-              <IDSCard className="ids-mb-4">
+              <IDSCard hideonm className="ids-mb-4">
                 <h3 className="ids-heading-2">I min region</h3>
                 <p className="ids-mb-4">
                   Här väljer du den mottagning där du önskar att boka tider. De
@@ -49,7 +49,11 @@ function BokaTid() {
                   </a>
                 </IDSLink>
               </IDSCard>
-              <IDSCard className="ids-mt-8" style={{ display: "block" }}>
+              <IDSCard
+                hideonm
+                className="ids-mt-8"
+                style={{ display: "block" }}
+              >
                 <h3 className="ids-heading-2">På Gimo vårdcentral, Gimo</h3>
                 <p className="ids-mb-4">
                   Här visas den mottagning där du är listad på. Om du vill ändra
@@ -61,7 +65,11 @@ function BokaTid() {
                 </IDSLink>
               </IDSCard>
 
-              <IDSCard className="ids-mt-8" style={{ display: "block" }}>
+              <IDSCard
+                hideonm
+                className="ids-mt-8"
+                style={{ display: "block" }}
+              >
                 <h3 className="ids-heading-2">Andra mottagningar</h3>
                 <p className="ids-mb-4">
                   Här finns bokningsmöjligheter för andra mottagingar som du har
